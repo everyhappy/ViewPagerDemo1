3f33b3e74e216d5d70135e1d148d74b0c85167bf
huangkun <huangkun@yulong.com>
2016-10-31 21:44:58 +0800

    viewpagerdemo1_code

---
+++ app/src/main/java/com/viewpagedemo2/pagestate.java
@@ -0,0 +1,13 @@
+package com.viewpagedemo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pagestate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagestate);
+    }
+}
